1f3ee78455c719ec4706377981fc556d54d8c374
steinnhauser <hausersteinn@gmail.com>
2022-09-27 09:51:37 +0200

    Remove link import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./construction.svg";
 import "./App.css";
 
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./components/home-page/Home";
 import UnderConstruction from "./components/under-construction/UnderConstruction";
